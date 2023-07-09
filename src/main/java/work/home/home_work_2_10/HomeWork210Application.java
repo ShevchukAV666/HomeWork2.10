5fd2e84bf17c79da1ef3f5383fdbdbfab4b73be3
ShevchukAV666 <666mog@mail.ru>
2023-07-09 19:27:02 +0500

    Коммит

---
+++ src/main/java/work/home/home_work_2_10/HomeWork210Application.java
@@ -0,0 +1,13 @@
+package work.home.home_work_2_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork210Application.class, args);
+	}
+
+}
